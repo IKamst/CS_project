433413ac2b982acc55ce640b7c9ea9dccc1d390f
Ella Collins <ellacollins@Ellas-MacBook-Pro.local>
2022-01-17 12:04:25 +0100

    slight change to baseline

--- read_sentences.py
+++ read_sentences.py
@@ -5,18 +5,18 @@ def read_file(in_file):
     sentences = []  # array of the sentences
     tagged_sents = []  # array of pairs of words and tags for each sentence
     with open(in_file) as f:
-        word, pos, tagged_sent = [], [],[]
+        word, sem, tagged_sent = [], [],[]
         for line in f.readlines():
             sp = line.strip().split('\t')
             if len(sp) > 1:
                 if '#' not in sp[0]:
                     word.append(sp[1])
-                    pos.append(sp[3])
+                    sem.append(sp[3])
                     tagged_sent.append((sp[1], sp[3]))
             elif len(word) > 0:
                 sentences.append(word)
                 tagged_sents.append(tagged_sent)
-                word, pos, tagged_sent = [], [],[]
+                word, sem, tagged_sent = [], [],[]
                 
         if len(word) > 0:
             tagged_sents.append(tagged_sent)
@@ -24,11 +24,11 @@ def read_file(in_file):
 
 '''
 format of the sentences in the file with example styling
-ID:     sp[0] styling 
+WORD:     sp[0] styling 
 FORM:   sp[1] styling 
 LEMMA:  sp[2] style 
-POS:    sp[3] EXG 
-FEAT:   sp[4] (s:ng\np)/np  
-HEAD:   sp[5] style.v.02  
-DEPREL: sp[6] [Patient,Agent]
+SEM:    sp[3] EXG 
+CCG:   sp[4] (s:ng\np)/np  
+WordNet synset:   sp[5] style.v.02  
+VerbNet role: sp[6] [Patient,Agent]
 '''
